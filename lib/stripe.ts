b76edec7b6c0b2e7e29067014b2ba2fb208051fa
Tianyang Chen <chenty@udel.edu>
2026-02-01 01:19:11 -0500

    Remove Stripe API version to use default

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,8 +1,6 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
-})
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
 // Credit packages configuration (USD)
 export const CREDIT_PACKAGES = {
